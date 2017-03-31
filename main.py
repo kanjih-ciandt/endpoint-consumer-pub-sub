f240fd9f34f6a4a06c1671862f514672e861a40e
Kanji Hara Neto <kneto@kneto-macbookpro.roam.corp.google.com>
2017-03-30 23:58:32 -0300

    changing api names and removed no necessary methods

--- main.py
+++ main.py
@@ -53,75 +53,14 @@ SUBSCRIBER_RESOURCE = endpoints.ResourceContainer(
 
 
 # [START greeting_api]
-@endpoints.api(name='greeting', version='v1')
+@endpoints.api(name='clientpubsub', version='v1')
 class GreetingApi(remote.Service):
-
-    @endpoints.method(
-        # This method does not take a request message.
-        message_types.VoidMessage,
-        # This method returns a GreetingCollection message.
-        GreetingCollection,
-        path='greetings',
-        http_method='GET',
-        name='greetings.list')
-    def list_greetings(self, unused_request):
-        return STORED_GREETINGS
-
-    # ResourceContainers are used to encapsuate a request body and url
-    # parameters. This one is used to represent the Greeting ID for the
-    # greeting_get method.
-    GET_RESOURCE = endpoints.ResourceContainer(
-        # The request body should be empty.
-        message_types.VoidMessage,
-        # Accept one url parameter: an integer named 'id'
-        id=messages.IntegerField(1, variant=messages.Variant.INT32))
-
-    @endpoints.method(
-        # Use the ResourceContainer defined above to accept an empty body
-        # but an ID in the query string.
-        GET_RESOURCE,
-        # This method returns a Greeting message.
-        Greeting,
-        # The path defines the source of the URL parameter 'id'. If not
-        # specified here, it would need to be in the query string.
-        path='greetings/{id}',
-        http_method='GET',
-        name='greetings.get')
-    def get_greeting(self, request):
-        try:
-            # request.id is used to access the URL parameter.
-            return STORED_GREETINGS.items[request.id]
-        except (IndexError, TypeError):
-            raise endpoints.NotFoundException(
-                'Greeting {} not found'.format(request.id))
-    # [END greeting_api]
-
-    # [START multiply]
-    # This ResourceContainer is similar to the one used for get_greeting, but
-    # this one also contains a request body in the form of a Greeting message.
-    MULTIPLY_RESOURCE = endpoints.ResourceContainer(
-        Greeting,
-        times=messages.IntegerField(2, variant=messages.Variant.INT32,
-                                    required=True))
-
-    @endpoints.method(
-        # This method accepts a request body containing a Greeting message
-        # and a URL parameter specifying how many times to multiply the
-        # message.
-        MULTIPLY_RESOURCE,
-        # This method returns a Greeting message.
-        Greeting,
-        path='greetings/multiply/{times}',
-        http_method='POST',
-        name='greetings.multiply')
-    def multiply_greeting(self, request):
-        return Greeting(message=request.message * request.times)
 
     @endpoints.method(
         SUBSCRIBER_RESOURCE,
         PullResponse,
-        path='requesters', http_method='GET',
-        name='requesters.get'
+        path='pull', http_method='GET',
+        name='pull.get'
     )
     def get_pull(self, request):
 
